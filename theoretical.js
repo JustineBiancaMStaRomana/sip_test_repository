9e7c6832324d6723bcacaf9161d43a73eef3a003
JustineBiancaMStaRomana <justinebianca.staromana@smc.pshs.edu.ph>
2025-03-31 15:39:17 +0800

    Add files via upload

---
+++ theoretical.js
@@ -0,0 +1,51 @@
+let output = document.getElementById("output");
+
+function recordStop(){
+    return new Promise((resolve) => setTimeout(resolve, 1500));
+}
+
+function recordStart(stream){
+    let recorder = new MediaRecorder(stream);
+    let data = [];
+
+    recorder.ondataavailable = (event) => data.push(event.data);
+    recorder.start();
+
+    let stopped = new Promise((resolve, reject) => {
+        recorder.onstop = resolve;
+        recorder.onerror = (event) => reject(event.name);
+    })
+
+    let recorded = recordStop().then(() => {
+        if (recorder.state === "recording") {
+            recorder.stop();
+        }
+    })
+
+    return Promise.all([stopped, recorded]).then(() => data);
+}
+
+function mediaShow(){
+    navigator.mediaDevices
+        .getUserMedia({
+            video: true,
+            audio: true,
+        })
+        .then((stream) => {
+            output.srcObject = stream;
+            output.captureStream = preview.captureStream || preview.mozCaptureStream;
+            return new Promise((resolve) => (output.onplaying = resolve));
+        })
+        .then(() => recordStart(output.captureStream()))
+        .then((recordedChunks) => {
+            let recordedBlob = new Blob(recordedChunks, {type: "video/webm"});
+            //google drive api upload goes here
+        })
+        .catch((error) => {
+            if (error.name === "NotFoundError") {
+              console.log("Camera or microphone not found. Can't record.");
+            } else {
+              console.log(error);
+            }
+          });
+}
